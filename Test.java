d62fb4ad586a5a763032b8559ec4b96912888227
vesakhaa <anandavesakha2905@gmail.com>
2025-10-04 10:23:37 +0700

    menulis baris kode baru

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+
+        System.out.println("Baris kode ini ditulis oleh Vesakha");
     }
 }
